c781fb8b786e87989702a164531e576ce15cd7b0
Danilo Silva <danilo.silva@msn.com>
2024-08-25 22:11:16 -0300

    Criando o módulo de Categorias

---
+++ src/app/pages/categorias/categorias-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoriaListaComponent } from './categoria-lista/categoria-lista.component';
+import { CategoriaFormComponent } from './categoria-form/categoria-form.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoriaListaComponent
+  },
+  {
+    path: 'novo',
+    component: CategoriaFormComponent
+  },
+  {
+    path: ':id/edicao',
+    component: CategoriaFormComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoriasRoutingModule { }
