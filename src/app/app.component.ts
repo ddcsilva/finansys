6f21a1cbfefefea0c5b314c44ff1d8ac1fe2c491
Danilo Silva <danilo.silva@msn.com>
2024-08-25 22:45:18 -0300

    Adicionando a barra de navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'finansys';
+  items = [
+    {
+      icon: "pi pi-fw pi-home",
+      routerLink: "/",
+      routerLinkActive: 'active-link'
+    },
+    {
+      label: "Relatórios",
+      routerLink: "/relatorios",
+      routerLinkActive: 'active-link'
+    },
+    {
+      label: "Lançamentos",
+      routerLink: "/lancamentos",
+      routerLinkActive: 'active-link'
+    },
+    {
+      label: "Categorias",
+      routerLink: "/categorias",
+      routerLinkActive: 'active-link'
+    }
+  ];
 }
